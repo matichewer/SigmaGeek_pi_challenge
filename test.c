427e97c4b4d24a29b59f240761414af31c9657a0
Matías David Schwerdt <schwerdt.matias@gmail.com>
2022-09-30 00:35:41 -0300

    change 9 digits to 21

--- test.c
+++ test.c
@@ -9,7 +9,7 @@ int main() {
     fp = fopen("one_millon.txt", "r");
         int palindrome_prime = 0; // 0 is false
     int num = 0;
-    int cant_digitos = 9;
+    int cant_digitos = 21;
 
     while(c != EOF && cant_digitos>0){
         c = fgetc(fp);
@@ -25,22 +25,25 @@ int main() {
 
     while(c != EOF){
         c = fgetc(fp);
+        int cant_dig;
         if((48<=c) && (c<=57)){
             num = remove_first_digit(num);
-            int cant_dig = hasNineDigits(num);
+            cant_dig = hasNineDigits(num);
             //printf("la cant de dig es: %d \n",cant_dig);
-            while(hasNineDigits(num)<8){
+            while(hasNineDigits(num)<21){
             //printf("Entre\n");
                 num = concatenate(num, c-'0');
                 c = fgetc(fp);
             }            
             num = concatenate(num,c-'0');
         }
-	printf("Siguiente numero a analizar: %i\n",num);
-	if(initial_check(num)){
-	    //palindrome_prime = checkNumber(num);
-        if(checkNumber(num))
-            printf("----------El numero %i es palindromo y primo----------\n",num);
+        //printf("Siguiente numero a analizar: %i\n",num);
+        if(initial_check(num)){
+            //palindrome_prime = checkNumber(num);
+            if(checkNumber(num)){
+                printf("----------El numero %i es palindromo y primo----------\n",num);
+                break;
+            }
         }
     }
 
--- test.h
+++ test.h
@@ -2,33 +2,36 @@ int hasNineDigits(int num){
     int toReturn = 0;
     int contador = 0;
     while(num!=0){
-	//printf("numero: %d\n",num);
-    	num = num / 10;
-	//printf("numero sin ult dig %d\n", num);
-	contador++;
+	    //printf("numero: %d\n",num);
+        num = num / 10;
+	    //printf("numero sin ult dig %d\n", num);
+        contador++;
     }
-    if(contador==9)
-	toReturn=1;
-
+    if(contador==21)
+        toReturn = 1;
     return contador;
 }
+
 int getFirstDigit(int num){
     while(num>=10){
         num = num / 10;
     }
     return num;
 }
+
 int getLastDigit(int num){
     return num % 10;
 }
+
 int initial_check(int num){
     int flag = 0;
     int firstDigit = getFirstDigit(num);
     int lastDigit = getLastDigit(num);
     return firstDigit == lastDigit;
 }
+
 int is_prime(int num){
-        int i, flag = 1;
+    int i, flag = 1;
 
     // Iterate from 2 to sqrt(n)
     for (i = 2; i <= sqrt(num); i++) {
@@ -73,9 +76,7 @@ unsigned checkNumber(unsigned number){
     int toReturn=0;
     if(is_palindrome(number) && is_prime(number))
         toReturn=1;
-
     return toReturn;
-
 }
 
 unsigned concatenate(unsigned x, unsigned y) {
@@ -91,5 +92,3 @@ unsigned int remove_first_digit(unsigned int n) {
     else
         return n%10 + remove_first_digit(n/10) * 10;
 }
-
-
