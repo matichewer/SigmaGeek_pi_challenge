ebcef84cbc3debf13303148347a346df399d8ced
Matías David Schwerdt <schwerdt.matias@gmail.com>
2022-09-30 00:55:03 -0300

    cant loop

--- test.c
+++ test.c
@@ -10,6 +10,7 @@ int main() {
         int palindrome_prime = 0; // 0 is false
     int num = 0;
     int cant_digitos = 21;
+    unsigned int cant_loop = 0;
 
     while(c != EOF && cant_digitos>0){
         c = fgetc(fp);
@@ -30,14 +31,20 @@ int main() {
             num = remove_first_digit(num);
             cant_dig = hasNineDigits(num);
             //printf("la cant de dig es: %d \n",cant_dig);
-            while(hasNineDigits(num)<21){
+            while(hasNineDigits(num)<20){
             //printf("Entre\n");
                 num = concatenate(num, c-'0');
                 c = fgetc(fp);
+                cant_loop = cant_loop + 1;
             }            
             num = concatenate(num,c-'0');
         }
-        //printf("Siguiente numero a analizar: %i\n",num);
+        /*
+        if(cant_loop%1000000 == 0){
+            printf("Analizando iteracion numero: %i\n", cant_loop);
+        }
+        */
+        printf("Siguiente numero a analizar: %i\n",num);
         if(initial_check(num)){
             //palindrome_prime = checkNumber(num);
             if(checkNumber(num)){
